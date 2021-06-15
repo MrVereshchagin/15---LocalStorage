30dcd2c0e0c7cdafe94cbf11344f20d327d31d9b
Igor Vereshchagin <9868425@gmail.com>
2021-06-15 23:57:39 +0300

    feat: add function populatelist

--- script.js
+++ script.js
@@ -5,15 +5,31 @@ const items = [];
 function addItem(e) {
     // отменяем действие браузера по умолчанию, т.е. отправку формы
     e.preventDefault();
+    // создаем объект со значением введенным в поле input
     const text = (this.querySelector('[name=item]')).value;
     const item = {
         text,
         done: false
     };
 
+    // пушим такие объекты в новый массив
     items.push(item);
+    // очищаем поле input
+    populateList(items, itemsList);
     this.reset();
-    console.log(item);
+}
+
+// пишем функцию заполненного списка, аргументы - новый пустой массив и ul с названиями 
+function populateList(plates = [], plateList) {
+    plateList.innerHTML = plates.map((plate, i) => {
+        return `
+            <li>
+                <input type='checkbox' data-index=${i} id='item${i}' ${plate.done ? 'checked' : ''} />
+                <label for='item${i}'>${plate.text}</label>
+            </li>
+        `;
+
+    }).join('');
 }
 
 addItems.addEventListener('submit', addItem);
